f2d7ebdcc0ed27571e6c2af655eeac538f7b1757
Hariraghav.S <hariraghava21s@gmail.com>
2025-08-13 23:29:00 +0530

    Updated cors policy-13-08-2025

--- src/transactionservice/backend/Server.js
+++ src/transactionservice/backend/Server.js
@@ -35,6 +35,14 @@ if (process.env.NODE_ENV !== "production") {
   const token=generateToken();
   console.log(token)
 }
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, PATCH");
+  res.header('Access-Control-Allow-Credentials',true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
 
 app.use("/transactionapi",sisproutes);
 app.use("/transactionapi",commissionRoutes);
